6817d8ae52a8f84656cbd6dc878bb6d7bffc79b3
User Ahmed <uchihamadara20488@gmail.com>
2025-06-29 00:02:09 +0300

    init

--- src/main/java/com/example/later_spring/item/ItemController.java
+++ src/main/java/com/example/later_spring/item/ItemController.java
@@ -2,6 +2,7 @@ package com.example.later_spring.item;
 
 
 import com.example.later_spring.item.dto.AddItemRequest;
+import com.example.later_spring.item.dto.GetItemRequest;
 import com.example.later_spring.item.dto.ItemDto;
 import com.example.later_spring.item.dto.ModifyItemRequest;
 import lombok.AccessLevel;
@@ -18,18 +19,18 @@ import java.util.List;
 public class ItemController {
     ItemService itemService;
 
-    /* @GetMapping
-     public List<ItemDto> get(
-             @RequestHeader("X-Later-User-Id") long userId,
-             @RequestParam(name = "state", defaultValue = "unread") String state,
-             @RequestParam(name = "contentType", defaultValue = "all") String contentType,
-             @RequestParam(name = "sort", defaultValue = "newest") String sort,
-             @RequestParam(name = "limit", defaultValue = "10") int limit,
-             @RequestParam(name = "tags", required = false) List<String> tags
-     ) {
-         return itemService.getItems(GetItemRequest.of(userId, state, contentType, sort, limit, tags));
-     }
- */
+    @GetMapping
+    public List<ItemDto> get(
+            @RequestHeader("X-Later-User-Id") long userId,
+            @RequestParam(name = "state", defaultValue = "unread") String state,
+            @RequestParam(name = "contentType", defaultValue = "all") String contentType,
+            @RequestParam(name = "sort", defaultValue = "newest") String sort,
+            @RequestParam(name = "limit", defaultValue = "10") int limit,
+            @RequestParam(name = "tags", required = false) List<String> tags
+    ) {
+        return itemService.getItems(GetItemRequest.of(userId, state, contentType, sort, limit, tags));
+    }
+
     @GetMapping(params = "lastName")
     public List<ItemDto> get(@RequestParam(name = "lastName") String lastName) {
         return itemService.getUserItems(lastName);
